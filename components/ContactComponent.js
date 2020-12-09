86c11cb10420e11ad18eae615c309821bba033d4
Bahige <bahigesaab8@gmail.com>
2020-12-09 23:12:24 +0200

    Email and Social Share

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
-import {Card} from 'react-native-elements'
+import {Card, Button, Icon} from 'react-native-elements'
 import *  as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 
 const ContactComponent = () => {
+
+    const sendEmail = () =>{
+        MailComposer.composeAsync({
+            recipients:['bahigesaab8@gmail.com'],
+            subject:"Enquiry",
+            body: "To whom it may concern: "
+        })
+    }
+
     return (
         <View style={{marginTop:"10%", marginBottom:"10%"}}>
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
@@ -17,6 +27,9 @@ const ContactComponent = () => {
                 <Text style={styles.addressItem}>Tel: +852 1234 5678</Text>
                 <Text style={styles.addressItem}>Fax: +852 8765 4321</Text>
                 <Text style={styles.addressItem}>Email:confusion@food.net</Text>
+                <Button title="Send Email" buttonStyle={{backgroundColor:"#512DA8"}}
+                icon={<Icon name='envelope-o' type='font-awesome' color='white'/>}
+                onPress= {()=> sendEmail()}/>
             </Card>
             </Animatable.View>
         </View>
